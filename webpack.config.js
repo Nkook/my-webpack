5b368d0ac42bd8fed50695631e6fc2d51a324438
Xiaoguai <xiaoguaien@126.com>
2022-11-02 20:11:22 +0800

    webpack核心概念之entry和output

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,23 @@
 const path = require('path');
 
 module.exports = {
- // 打包的入口文件
- entry: './src/index.js',
- // 打包输出的文件地址，输出到当前文件夹的dist目录，输出的文件名称叫bundles.js
+ // 单入口文件
+ // // 打包的入口文件
+ // entry: './src/index.js',
+ // // 打包输出的文件地址，输出到当前文件夹的dist目录，输出的文件名称叫bundles.js
+ // output: {
+ //   path: path.join(__dirname, 'dist'),
+ //   filename: 'bundles.js'
+ // },
+
+ // 多入口文件 npm run build后，dist文件夹下会生成index.js、search.js两个文件
+ entry: {
+   index: './src/index.js',
+   search: './src/search.js'
+ },
  output: {
    path: path.join(__dirname, 'dist'),
-   filename: 'bundles.js'
+   filename: '[name].js'
  },
  // 环境：线上环境
  mode: 'production'
