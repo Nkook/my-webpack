9425cfc0889e999f1010217f7748c4f5ea7a9f36
Xiaoguai <xiaoguaien@126.com>
2023-02-02 20:22:06 +0800

    file-loader解析字体

--- webpack.config.js
+++ webpack.config.js
@@ -46,6 +46,10 @@ module.exports = {
       {
         test: /.(png|jpg|gif|jpeg)$/,
         use: 'file-loader'
+      },
+      {
+        test: /.(woff|woff2|eot|ttf|otf)$/,
+        use: 'file-loader'
       }
     ]
   }
